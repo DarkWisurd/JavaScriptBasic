a15c039029f47bfcfdb47f58fb355d8a6db98559
DeshpandeVedant37 <deshpandevedant37@gmail.com>
2021-03-23 16:23:50 +0530

    added everithing about arrays and did a coding challenge also added an object

--- JavaScript_Fundamentals2/script.js
+++ JavaScript_Fundamentals2/script.js
@@ -236,4 +236,77 @@ console.log(ages)
 //Basic Array Operations (Methods)
 
 
+// add elements
+const friends2 = ["vedant", "soham", "atharva"];
+friends2.push("jay")// .push is an function so it can return something 
+const sup = friends2.push("arjun")
 
+console.log(sup)// it returns length of an array
+console.log(friends2)
+
+friends2.unshift("running out of names");
+console.log(friends2);
+
+// remove elements
+
+friends2.pop();// will remove last element of array
+
+
+friends2.pop();// will return the last element of an array
+console.log(friends2)
+
+friends2.shift();//removes the frirst element of an array 
+// will also return the element that was removed 
+console.log(friends2);
+
+console.log(friend2.indexOf("soham"));
+
+console.log(friend2.includes("vedant"))
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+TEST DATA: 125, 555 and 44
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+GOOD LUCK 😀
+*/
+
+const bill = 275;
+// const tip = bill >= 50 ? bill / 100 * 15 : bill / 100 * 15;
+// console.log(`bill=${bill} tip = ${tip} total amount = ${bill + tip}`)
+
+function calcTip(bill) {
+    const tip = bill >= 50 ? bill / 100 * 15 : bill / 100 * 15;
+    return tip;
+}
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])]
+// bonus
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(totals);
+console.log(tips)
+
+
+
+
+
+
+//Introduction to Objects
+
+const vedant1 = {
+    firstName: "vedant",
+    lastName: "deshpande",
+    age: 2037 - 2007,
+    job: "student",
+    friends: ["vedant", "soham", "atharva"]
+}
+console.log(vedant1)
