47a33b3f412421040a34ddd38caf741005a61a75
DeshpandeVedant37 <deshpandevedant37@gmail.com>
2021-03-27 17:10:48 +0530

    added everithing about loops and did an coding challenge about the same and also ended fundamentals 2

--- JavaScript_Fundamentals2/script.js
+++ JavaScript_Fundamentals2/script.js
@@ -449,3 +449,165 @@ else {
     console.log("Jhon's BMI is higher")
 }
 
+//  Iteration: The for Loop
+//without loop    
+// console.log("lefting weights Repetation 1");
+// console.log("lefting weights Repetation 2");
+// console.log("lefting weights Repetation 3");
+// console.log("lefting weights Repetation 4");
+// console.log("lefting weights Repetation 5");
+// console.log("lefting weights Repetation 6");
+// console.log("lefting weights Repetation 7");
+// console.log("lefting weights Repetation 8");
+// console.log("lefting weights Repetation 9");
+// console.log("lefting weights Repetation 10");
+// with for Loop
+
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log("lefting weights Repetation", rep);
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    const number = 19;
+    console.log(number * i)
+}
+
+
+
+//Looping Arrays, Breaking and Continuing
+const types = [];
+
+const vedantArray = [
+    "vedant",
+    "deshpande",
+    2037 - 2007,
+    "student",
+    ["arjun", "perter", "steven"]
+];
+for (let i = 0; i < vedantArray.length; i++) {
+    console.log(vedantArray[i], typeof vedantArray[i])
+    // filling array
+    types[i] = typeof vedantArray[i];
+}
+console.log(types)
+
+
+const yearsArray = [2007, 1991, 1964, 2021];
+const ages5 = [];
+for (let i = 0; i < yearsArray.length; i++) {
+    2037 - yearsArray[i];
+
+    ages5.push(2037 - yearsArray[i]);
+}
+console.log(ages5)
+
+
+// statements for loops (continiue and break )
+// continue
+
+
+for (let i = 0; i < vedantArray.length; i++) {
+    if (typeof vedantArray[i] !== "string") continue;
+    console.log(vedantArray[i], typeof vedantArray[i])
+}
+
+// break
+console.log("-- BREAK WITHOUT NUMBER --")
+for (let i = 0; i < vedantArray.length; i++) {
+    if (typeof vedantArray[i] === "number") break;
+    console.log(vedantArray[i], typeof vedantArray[i])
+}
+
+
+//Looping Backwards and Loops in Loops
+
+
+// LOOPING BACKWARDS 
+
+for (let i = vedantArray.length - 1; i >= 0; i--) {
+    console.log(vedantArray[i]);
+}
+
+// loop inside of loop
+
+
+for (let exercise = 1; exercise <= 3; exercise++) {
+    console.log(`-------- Starting Exercise ${exercise} `)
+    // This is me complicating the code by adding if-else
+    for (let rep = 1; rep <= 5; rep++) {
+        if (exercise === 1) {
+            console.log(`Lifting weight repetation ${rep}`);
+        }
+        else if (exercise === 2) {
+            console.log(`Push Up repetation ${rep}`)
+        }
+        else {
+            console.log(`yoga repetation ${rep}`)
+        }
+    }
+
+}
+
+// The while loop
+console.log("---The While Loop---");
+
+let rep = 1;
+while (rep <= 10) {
+    console.log("lefting weights Repetation", rep);
+    rep++
+}
+//dont know what Math.random is
+let dice = Math.trunc(Math.random() * 6) + 1;
+while (dice !== 6) {
+    console.log(` you rooled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) {
+        console.log("woohoo you rolled a 6")
+    }
+}
+
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+GOOD LUCK 😀
+*/
+
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
