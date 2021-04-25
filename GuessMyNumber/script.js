84fb46a26d68d042f4fe1aa5319c8b15c1b46293
DeshpandeVedant37 <deshpandevedant37@gmail.com>
2021-04-25 13:07:16 +0530

    Added Basic Game logic to The game "Guess the Number"

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,2 +1,52 @@
-'use strict';
-console.log(document.querySelector(".message").textContent);
+//Selecting and Manipulating Elements//
+
+// 'use strict';
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "🎉 correct number! "
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 20;
+
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value)
+
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number((document.querySelector(".guess").value));
+    console.log(guess);
+    if (!guess) {
+        document.querySelector(".message").textContent = "⛔ No Number"
+    }
+    else if (guess === secretNumber) {
+        document.querySelector(".message").textContent = "🎉 correct number!"
+    }
+    else if (guess > secretNumber) {
+
+        if (score > 1) {
+
+            document.querySelector(".message").textContent = "📈 Too High!";
+            score--;
+            document.querySelector(".score").textContent = score;
+        }
+        else {
+            document.querySelector(".message").textContent = "💥 You lost the game"
+            document.querySelector(".score").textContent = 0;
+        }
+    }
+    else if (guess < secretNumber) {
+
+        if (score > 1) {
+            document.querySelector(".message").textContent = "📉 Too Low!"
+            score--;
+            document.querySelector(".score").textContent = score;
+        }
+        else {
+            document.querySelector(".message").textContent = "💥 You lost the game"
+            document.querySelector(".score").textContent = 0;
+        }
+    }
+
+})
---
+++ HTML_CSS/script.js
@@ -0,0 +1,9 @@
+let username = document.querySelector(".Username");
+let password = document.querySelector(".Password");
+if (username === "vedant" && password === "Vedant@37") {
+    alert("Correct!");
+
+}
+else {
+    alert("Username or password incorrect ")
+}
