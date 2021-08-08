5aac794999694a2f16939c05c2974d2eeced94a8
DeshpandeVedant37 <deshpandevedant37@gmail.com>
2021-08-08 14:40:09 +0530

    Modal window

---
+++ modalWindow/script.js
@@ -0,0 +1,26 @@
+'use strict';
+//defining classes
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+function openModal() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden")
+}
+function closeModal() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", openModal);//opning modal window
+    btnCloseModal.addEventListener("click", closeModal);//closing modal window
+    overlay.addEventListener("click", closeModal);//closing modal window
+    document.addEventListener("keydown", function (event) {
+        console.log(event.key);
+        if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+            closeModal()
+            console.log("Esc was pressed");
+        }
+    })
+}
