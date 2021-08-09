80a57d3cb5d88abd3901e2c1bfbf7d698121317f
DeshpandeVedant37 <deshpandevedant37@gmail.com>
2021-08-09 11:52:01 +0530

    Pig game!

--- modalWindow/script.js
+++ modalWindow/script.js
@@ -4,6 +4,7 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
+//functions to inpliment DRY 
 function openModal() {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden")
@@ -16,6 +17,7 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
     btnsOpenModal[i].addEventListener("click", openModal);//opning modal window
     btnCloseModal.addEventListener("click", closeModal);//closing modal window
     overlay.addEventListener("click", closeModal);//closing modal window
+    //usnig Esc key to close modal window
     document.addEventListener("keydown", function (event) {
         console.log(event.key);
         if (event.key === "Escape" && !modal.classList.contains("hidden")) {
